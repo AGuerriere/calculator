ebaea94f7db28b4d10340de91c42c57cccfdd1b4
Antonio Guerriere <antonioguerriere@gmail.com>
2023-08-15 17:05:19 +0200

    wip 2

--- main.js
+++ main.js
@@ -1,37 +1,50 @@
 const numberButtons = document.querySelectorAll("[data-number]")
 const operandButtons = document.querySelectorAll("[data-operand]")
-const equalButton = document.querySelector("data-equal")
-const allClearButton = document.querySelector("data-all-clear")
-const previousOperationDisplay = document.querySelector("data-previous-operation")
-const currentOperationDisplay = document.querySelector("data-current-operation")
+const equalButton = document.querySelector("[data-equal]")
+const allClearButton = document.querySelector("[data-all-clear]")
+const previousOperationDisplay = document.querySelector("[data-previous-operation]")
+const currentOperationDisplay = document.querySelector("[data-current-operation]")
+
+let displayedNum = "";
+
+let currentNumber = ""
+
+let operandButtonPressed = false
+
+// Clear Button
+allClearButton.addEventListener("click", () => {
+  previousOperationDisplay.textContent = ""
+  currentOperationDisplay.textContent = ""
+  displayedNum = ""
+})
 
 
-class Calculator {
-  constructor(previousOperationDisplay, currentOperationDisplay){
-    this.currentOperationDisplay = currentOperationDisplay
-    this.previousOperationDisplay = previousOperationDisplay
-  }
 
-    clear(){
-
+// Append numbers when clicked
+numberButtons.forEach((number) => {
+  number.addEventListener("click", () => {
+    if (currentOperationDisplay.textContent.length <= 14){
+      displayedNum += number.textContent;
+      currentOperationDisplay.textContent = displayedNum;
     }
+  })
+})
 
-    delete(){
+// Add event listener for each opearand
+operandButtons.forEach((operand) => {
+  operand.addEventListener("click", () => {
+    switch(operand.textContent){
+      case "+":
+        add()
+      case "-":
+        console.log('Operands')
+      case "÷":
+        console.log('Operands')
+      case "*":
+        console.log('Operands')
+  }})
+})
 
-    }
-    
-    appendNumber(){
-
-    }
-    chooseOperation(operation){
-
-    }
-
-    compute(){
-
-    }
-
-    updateDisplay(){
-      
-    }
+function add() {
+  currentNumber += currentOperationDisplay.textContent
 }
